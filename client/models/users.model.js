49f127a494227d4ea15c9356e19f45cef34f4a22
Raditya <arya.raditya26@gmail.com>
2017-02-15 20:45:51 +0700

    create users model

---
+++ client/models/users.model.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const mongoose = require('mongoose');
+var Schema     = mongoose.Schema;
+
+var usersSchema = new Schema({
+  username  : { type: String, required: true},
+  email     : { type: String, required: true},
+  password  : { type: String, required: true},
+},
+{
+  timestamps : true
+})
+
+var Users = mongoose.model('Users', usersSchema)
+
+module.exports = Users;
